07c5fa2a25990169287238339167ccd03186a431
usermac <4321>
2022-04-05 22:45:29 +0500

    Initial commit

---
+++ app/src/main/java/ru/ama/ottest/presentation/GameFragment.kt
@@ -0,0 +1,135 @@
+package ru.ama.ottest.presentation
+
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ru.ama.ottest.R
+import ru.ama.ottest.databinding.FragmentGameBinding
+import ru.ama.ottest.domain.entity.Level
+
+class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
+    private lateinit var binding: FragmentGameBinding
+
+    private lateinit var level: Level
+    private var optionsTextViews = mutableListOf<TextView>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[GameViewModel::class.java]
+        getTextViewsOptions()
+        setupClickListenersToOptions()
+        observeViewModel()
+        if (savedInstanceState == null) {
+            viewModel.startGame(level)
+        }
+    }
+
+    private fun parseArgs() {
+        val args = requireArguments()
+        if (!args.containsKey(ARG_LEVEL)) {
+            throw RuntimeException("Required param level is absent")
+        }
+        args.getParcelable<Level>(ARG_LEVEL)?.let {
+            level = it
+        }
+    }
+
+    private fun getTextViewsOptions() {
+        with(binding) {
+            optionsTextViews.add(tvOption1)
+            optionsTextViews.add(tvOption2)
+            optionsTextViews.add(tvOption3)
+            optionsTextViews.add(tvOption4)
+            optionsTextViews.add(tvOption5)
+            optionsTextViews.add(tvOption6)
+        }
+    }
+
+    private fun setupClickListenersToOptions() {
+        for (textView in optionsTextViews) {
+            textView.setOnClickListener {
+                viewModel.chooseAnswer(textView.text.toString().toInt())
+            }
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.question.observe(viewLifecycleOwner) {
+            with(binding) {
+                tvSum.text = it.sum.toString()
+                tvLeftNumber.text = it.visibleNumber.toString()
+                setupTextToOptions(it.answers)
+            }
+        }
+        viewModel.leftFormattedTime.observe(viewLifecycleOwner) {
+            binding.tvTimer.text = it
+        }
+        viewModel.gameResult.observe(viewLifecycleOwner) {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, GameFinishedFragment.newInstance(it))
+                .addToBackStack(null)
+                .commit()
+        }
+        viewModel.percentOfRightAnswers.observe(viewLifecycleOwner) {
+            binding.progressBar.setProgress(it, true)
+        }
+        viewModel.enoughPercentage.observe(viewLifecycleOwner) {
+            setupProgressColorByState(it)
+        }
+        viewModel.minPercentOfRightAnswers.observe(viewLifecycleOwner) {
+            binding.progressBar.secondaryProgress = it
+        }
+    }
+
+    private fun setupTextToOptions(answers: List<Int>) {
+        for (i in answers.indices) {
+            optionsTextViews[i].text = answers[i].toString()
+        }
+    }
+
+    private fun setupProgressColorByState(enoughPercentage: Boolean) {
+        val colorResId = if (enoughPercentage) {
+            android.R.color.holo_green_light
+        } else {
+            android.R.color.holo_red_light
+        }
+        val color = ContextCompat.getColor(requireContext(), colorResId)
+        binding.progressBar.progressTintList = ColorStateList.valueOf(color)
+    }
+
+    companion object {
+
+        private const val ARG_LEVEL = "level"
+        const val NAME = "GameFragment"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(ARG_LEVEL, level)
+                }
+            }
+        }
+    }
+}
